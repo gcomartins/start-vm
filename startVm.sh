148ae2cf84003acb6eec5803569e75ac57bfa74f
Guilherme Carneiro <gcomartins@gmail.com>
2022-05-05 20:03:55 -0300

    Update startVm.sh

--- startVm.sh
+++ startVm.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo "Definindo permissão do usuário ubuntu..."
+sudo chmod -aG sudo ubuntu
 
+echo "Defina a senha do usuário Ubuntu"
 sudo passwd ubuntu
 
-urubu100
-urubu100
-
 sudo apt upgrade && sudo apt update -y
+
+sudo apt-get install xrdp lxde-core lxde tigervnc-standalone-server -y
